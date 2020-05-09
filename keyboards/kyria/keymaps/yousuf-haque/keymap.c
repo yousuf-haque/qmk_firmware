e7bf3b62d9c6bf61fa0b640923469142ccbcbf79
Yousuf Haque <yousuf.s.haque@gmail.com>
2020-05-09 18:50:41 -0400

    Swap direction of encoder

--- keyboards/kyria/keymaps/yousuf-haque/keymap.c
+++ keyboards/kyria/keymaps/yousuf-haque/keymap.c
@@ -202,17 +202,17 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     }
     else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
             tap_code(KC_PGUP);
+        } else {
+            tap_code(KC_PGDN);
         }
     }
 }
