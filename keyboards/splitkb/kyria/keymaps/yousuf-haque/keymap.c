0f01f2539d05e5182b5437f8f2f15284871c5652
Yousuf Haque <yousuf.s.haque@gmail.com>
2022-06-16 01:32:53 -0400

    Make number layer delete forward by word

--- keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_QWERTY), KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
       _______, _______, S(G(KC_LEFT)), S(A(KC_LEFT)), S(A(KC_RIGHT)),  S(G(KC_RIGHT)),                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TD(TD_SMCL_NUMS), A(KC_UP),
       _______, _______, _______, S(KC_LEFT), S(KC_RIGHT), KC_MUTE, _______, _______, _______, _______, G(KC_LEFT), A(KC_LEFT), A(KC_RIGHT),  G(KC_RIGHT) , _______, A(KC_DOWN),
-                                 _______, _______, _______, _______, _______, _______, A(KC_BSPC), KC_DEL, _______, KC_MPRV
+                                 _______, _______, _______, _______, _______,      _______, A(KC_BSPC), A(KC_DEL), _______, KC_MPRV
     ),
 /*
  * Adjust Layer: Function keys, RGB
