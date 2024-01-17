da8e21b1999090a4f37537d5e222dd622143adfb
yousuf-haque <yousuf.s.haque@gmail.com>
2024-01-16 19:30:02 -0500

    Fix thumb cluster

--- keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+                                KC_MPLY , SC_LAPO, MT(MOD_LGUI, KC_BSPC), LT(_NAV, KC_SPC) , LT(_SYM, KC_ENT)   ,     LT(_SYM, KC_ENT)    , LT(_NAV, KC_BSPC) , MT(MOD_RGUI, KC_SPC), SC_RAPC, KC_MNXT
     ),
 
 
