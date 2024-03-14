123b13b1c1afa57bd98456718b66f11ce176c917
yousuf-haque <yousuf.s.haque@gmail.com>
2024-03-14 15:37:14 -0400

    Replace ctrl + tab with alt + tab

--- keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
@@ -32,7 +32,7 @@ enum layers {
 #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
-#define CTL_TAB MT(MOD_LCTL, KC_TAB)
+#define ALT_TAB MT(MOD_LALT, KC_TAB)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
+     ALT_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , LM(_NAV, MOD_LGUI),KC_CAPS,     FKEYS  , LM(_NAV, MOD_LGUI), KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 KC_MPLY , SC_LCPO, MT(MOD_LGUI, KC_BSPC), LT(_NAV, KC_SPC) , LT(_SYM, KC_ENT)   ,     LT(_SYM, KC_ENT)    , LT(_NAV, KC_BSPC) , MT(MOD_RGUI, KC_SPC), SC_RAPC, KC_MNXT
     ),
