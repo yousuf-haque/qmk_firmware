72fd783366df135057dec6586f2c516234163b9a
yousuf-haque <yousuf.s.haque@gmail.com>
2024-03-14 15:30:58 -0400

    Add underscore to symbol home row

--- keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_SYM] = LAYOUT(
     TO(_QWERTY), KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                                               _______, _______, _______, _______, _______, KC_BSLS,
-      _______, KC_HASH, KC_DLR, KC_LBRC, KC_RBRC, KC_GRV,                                                   KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOTE,
+      _______, KC_HASH, KC_DLR, KC_LBRC, KC_RBRC, KC_GRV,                                                   KC_PLUS, KC_MINS, S(KC_MINS), KC_ASTR, KC_PERC, KC_QUOTE,
       _______, KC_PERC, KC_CIRC, S(KC_COMM), S(KC_DOT), KC_TILD, _______, _______,         _______, _______, KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
                                  _______, _______, _______, S(KC_TAB), KC_TAB,            S(KC_TAB),  KC_TAB, KC_DEL, _______, KC_MUTE
     ),
