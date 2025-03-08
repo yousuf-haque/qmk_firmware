e6afaf35c4d9d29dcb2681a5eb99c52e19903e0d
Yousuf Haque <yousuf.s.haque@gmail.com>
2025-03-08 16:23:36 -0500

    Swap number row for top navigation row

--- keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yousuf-haque/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                KC_MPLY , SC_LAPO, MT(MOD_LGUI, KC_BSPC), LT(_NAV, KC_SPC) , LT(_SYM, KC_ENT)   ,     LT(_SYM, KC_ENT)    , LT(_NAV, KC_BSPC) , MT(MOD_RGUI, KC_SPC), SC_RAPC, KC_MNXT
+                                KC_MPLY , SC_LCPO, MT(MOD_LGUI, KC_BSPC), LT(_NAV, KC_SPC) , LT(_SYM, KC_ENT)   ,     LT(_SYM, KC_ENT)    , LT(_NAV, KC_BSPC) , MT(MOD_RGUI, KC_SPC), SC_RAPC, KC_MNXT
     ),
 
 
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_VOLU, KC_DEL,
+      _______,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_DEL,
       _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_INS,
       _______, _______, _______, _______, _______, _______, _______, KC_SCRL, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
